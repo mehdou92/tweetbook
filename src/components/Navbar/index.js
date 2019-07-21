dc4965ab04bcb3ffd5b56e0b7d3b5211ac8980f6
benjamin <kazmierczakbenjamin@gmail.com>
2019-07-21 13:21:51 +0200

    fix style btn menu

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -33,7 +33,7 @@ export default function Navbar() {
           </div>
           <div class="block lg:hidden">
             <button class="flex items-center px-3 py-2 border rounded text-bluegit-500 border-blue-500 hover:text-blue-700 hover:border-blue-700">
-              <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" /></svg>
+              <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path fill="#4299e1" d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" /></svg>
             </button>
           </div>
           <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
