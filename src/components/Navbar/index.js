48b28de0b7ebac254ae090ba57b18f34dd1997e6
benjamin <kazmierczakbenjamin@gmail.com>
2019-07-21 12:47:23 +0200

    Update color menu

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -39,9 +39,9 @@ export default function Navbar() {
           <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
             <div class="text-sm lg:flex-grow flex">
               <Link to="/">
-                <button class="flex bg-transparent hover:bg-gray-100 text-blue-900 font-semibold py-2 px-4">
+                <button class="flex bg-transparent hover:bg-blue-900 font-semibold py-2 px-4">
 
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z"/><path d="M0 0h24v24H0z" fill="none"/></svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z" fill="#2b6cb0"/><path d="M0 0h24v24H0z" fill="none"/></svg>
                 </button>
               </Link>
               {
@@ -49,15 +49,15 @@ export default function Navbar() {
                   ?
                   <>
                     <Link to={`/profile/${user.username}`}>
-                    <button class=" flex bg-transparent hover:bg-gray-100 text-blue-900 font-semibold py-2 px-4">
+                    <button class=" flex bg-transparent hover:bg-blue-900 text-blue-900 font-semibold py-2 px-4">
 
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"/><path d="M16 11c1.66 0 2.99-1.34 2.99-3S17.66 5 16 5c-1.66 0-3 1.34-3 3s1.34 3 3 3zm-8 0c1.66 0 2.99-1.34 2.99-3S9.66 5 8 5C6.34 5 5 6.34 5 8s1.34 3 3 3zm0 2c-2.33 0-7 1.17-7 3.5V19h14v-2.5c0-2.33-4.67-3.5-7-3.5zm8 0c-.29 0-.62.02-.97.05 1.16.84 1.97 1.97 1.97 3.45V19h6v-2.5c0-2.33-4.67-3.5-7-3.5z"/></svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"/><path fill="#2b6cb0" d="M16 11c1.66 0 2.99-1.34 2.99-3S17.66 5 16 5c-1.66 0-3 1.34-3 3s1.34 3 3 3zm-8 0c1.66 0 2.99-1.34 2.99-3S9.66 5 8 5C6.34 5 5 6.34 5 8s1.34 3 3 3zm0 2c-2.33 0-7 1.17-7 3.5V19h14v-2.5c0-2.33-4.67-3.5-7-3.5zm8 0c-.29 0-.62.02-.97.05 1.16.84 1.97 1.97 1.97 3.45V19h6v-2.5c0-2.33-4.67-3.5-7-3.5z"/></svg>
                     </button>
                     </Link>
                     <Link to="/logout">
-                    <button  onClick={handleSignOut} class="flex bg-transparent hover:bg-gray-100 text-blue-900 font-semibold py-2 px-4">
+                    <button  onClick={handleSignOut} class="flex bg-transparent hover:bg-blue-900 text-blue-900 font-semibold py-2 px-4">
 
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="none" d="M0 0h24v24H0z"/><path d="M13 3h-2v10h2V3zm4.83 2.17l-1.42 1.42C17.99 7.86 19 9.81 19 12c0 3.87-3.13 7-7 7s-7-3.13-7-7c0-2.19 1.01-4.14 2.58-5.42L6.17 5.17C4.23 6.82 3 9.26 3 12c0 4.97 4.03 9 9 9s9-4.03 9-9c0-2.74-1.23-5.18-3.17-6.83z"/></svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="none" d="M0 0h24v24H0z"/><path fill="#2b6cb0" d="M13 3h-2v10h2V3zm4.83 2.17l-1.42 1.42C17.99 7.86 19 9.81 19 12c0 3.87-3.13 7-7 7s-7-3.13-7-7c0-2.19 1.01-4.14 2.58-5.42L6.17 5.17C4.23 6.82 3 9.26 3 12c0 4.97 4.03 9 9 9s9-4.03 9-9c0-2.74-1.23-5.18-3.17-6.83z"/></svg>
                 </button>
                     </Link>
                   </>
@@ -79,7 +79,7 @@ export default function Navbar() {
                 { console.log("Nav Bar isLogged : ", isLogged())} */}
             </div>
             <div>
-              <a href="#" class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0">Download</a>
+              <a href="#" class="inline-block text-sm px-4 py-2 leading-none rounded bg-blue-500 text-white hover:border-transparent hover:bg-blue-700 mt-4 lg:mt-0">Download</a>
             </div>
           </div>
         </nav>
