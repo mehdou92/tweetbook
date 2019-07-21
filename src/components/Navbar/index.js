dafc8d71af3fe5a484abac969000e110047349c3
benjamin <kazmierczakbenjamin@gmail.com>
2019-07-21 16:22:16 +0200

    navbar relief

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -31,11 +31,6 @@ export default function Navbar() {
             <svg class="fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z" /></svg>
             <span class="font-semibold text-xl tracking-tight">TweetBook</span>
           </div>
-          <div class="block lg:hidden">
-            <button class="flex items-center px-3 py-2 border rounded text-bluegit-500 border-blue-500 hover:text-blue-700 hover:border-blue-700">
-              <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path fill="#4299e1" d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" /></svg>
-            </button>
-          </div>
           <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
             <div class="text-sm lg:flex-grow flex">
               <Link to="/">
@@ -89,9 +84,6 @@ export default function Navbar() {
               }
               {/* {console.log("Nav Bar user : ", user)}
                 { console.log("Nav Bar isLogged : ", isLogged())} */}
-            </div>
-            <div>
-              <a href="#" class="inline-block text-sm px-4 py-2 leading-none rounded bg-blue-500 text-white hover:border-transparent hover:bg-blue-700 mt-4 lg:mt-0">Download</a>
             </div>
           </div>
         </nav>
